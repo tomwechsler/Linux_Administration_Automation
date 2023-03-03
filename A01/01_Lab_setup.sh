c2f9ca229164afeac55fa9435cff21879fbf366c
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-03 17:35:47 +0100

    new files

---
+++ A01/01_Lab_setup.sh
@@ -0,0 +1,12 @@
+sudo apt install -y vim git cpu-checker
+
+#Check for virtualiztion supprt
+sudo kvm-ok
+
+#Clone the repo
+git clone https://github.com/tomwechsler/Linux_Administration_Automation.git
+
+
+
+For ease of access to the same virtual machines as on the labs we recommend using
+Vagrant and VirtualBox. These are available free of change on Linux, MacOS and Windows.
---
+++ A02/01_Working_with_branches.sh
@@ -0,0 +1,18 @@
+#Working on ubuntu
+
+
+cd ~/comptia-automation
+git status
+git branch
+cat ~/comptia-automation/shellscripting/hello.sh
+git branch -a
+git checkout scriptarg
+cat ~/comptia-automation/shellscripting/hello.sh
+git checkout main
+
+
+
+We have already used the git download to retrieve the Vagrantfile, allowing us consistency in
+the VM builds.
+We will work in craeting out own branches, but we have 3 branches in this repo which we can
+use to see different versions of the same bash script.
