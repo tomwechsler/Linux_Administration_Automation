2c41340d830a47b02988d1a4b98bd903efe301d2
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-03 06:53:41 +0100

    new files

---
+++ Default_config_rhel_systems.sh
@@ -0,0 +1,8 @@
+#If you get some locale warnings use:
+export LC_ALL="en_US.UTF-8"
+
+#If tab completion for systemctl does dit work
+sudo dnf install -y bash-completion
+
+#Install the vim enhanced editor
+sudo dnf install -y vim-enhanced
---
+++ Links.md
@@ -0,0 +1,13 @@
+# Some helpful links
+
+<https://www.virtualbox.org/>
+
+<https://www.vagrantup.com/>
+
+<https://www.ansible.com/>
+
+<https://ubuntu.com/>
+
+<https://www.opensuse.org/>
+
+<https://www.redhat.com/de>
