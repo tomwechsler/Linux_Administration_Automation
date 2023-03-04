1e77d6aa6d9b02430d1c1b17e8bf1508358cf253
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-04 16:35:20 +0100

    new files

--- A02/01_Working_with_branches.sh
+++ A02/01_Working_with_branches.sh
@@ -1,14 +1,57 @@
 #Working on ubuntu
 
+#Nothing at the moment
+ls
 
-cd ~/comptia-automation
+#Clone the repo
+git clone https://github.com/tomwechsler/Linux_Administration_Automation.git
+
+ls
+
+git status #Does not work
+
+#Change into the directory
+cd ~/Linux_Administration_Automation
+
+#We are working at the main branch
 git status
+
+#The .git and the repo metadata
+ls .git
+
+#Start an example, why we use branches
+cat /shellscripting/hello.sh
+
+git status
+
+#The branch (this is local)
 git branch
-cat ~/comptia-automation/shellscripting/hello.sh
+
+#The remote branches
 git branch -a
+
+#Switch the branch
 git checkout scriptarg
-cat ~/comptia-automation/shellscripting/hello.sh
+
+git branch
+
+#Lets look at the script
+cat ~/Linux_Administration_Automation/shellscripting/hello.sh
+
+#Switch the branch
+git checkout scriptread
+
+git branch
+
+#Lets look at the script
+cat ~/Linux_Administration_Automation/shellscripting/hello.sh
+
+#Switch back the main branch
 git checkout main
+
+#Lets look at the script
+cat ~/Linux_Administration_Automation/shellscripting/hello.sh
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Linux Administration: Automation
 Linux automation with three different distributions (Ubuntu, openSUSE and Red Hat)!
+
+## Content of the sections
+
+| Sections | Section description             |
+|-----|-------------------------------------------|
+| A01 | Lab setup |
+| A02 | Version Control Using Git |
+| A03 | Linux Automation Using Shell Scripts |
+| A04 | Implementing Configuration Management and IaC |
+| A05 | Ansible for Configuration Management  |
+| A06 | Puppet for Configuration Management |
+| A07 | SaltStack for Configuration Management |
+| A08 | Chef for Configuration Management |
+| A09 | Terraform to Deploy Your Virtual Machines |
+| A10 | Implementing Containers Using Docker |
+| A11 | Managing Container Micro-services Using Kubernetes |
