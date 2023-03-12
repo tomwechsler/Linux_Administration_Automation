7716d14e2a49771af99a60a3ae0d3b6dfa3f3b65
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-12 14:24:35 +0100

    lines edited

--- A04/01_Syntax_control_using_modeline.sh
+++ A04/01_Syntax_control_using_modeline.sh
@@ -39,6 +39,6 @@ vagrant up
 
 
 Vagrant, as we have seen, can be used to deploy systems. This provides the OS,
-using provisioners, we can further configre the system. The file format of a
+using provisioners, we can further configure the system. The file format of a
 Vagrantfile is Ruby. Adding a modeline can help vi/vim understand the syntax
 and format.
