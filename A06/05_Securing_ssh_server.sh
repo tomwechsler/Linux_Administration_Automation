538863c8fd55df856c60733ac216e7d861e7b377
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-14 12:31:26 +0100

    new files

---
+++ A06/05_Securing_ssh_server.sh
@@ -0,0 +1,31 @@
+#Working on ubuntu
+
+#Create a new manifest
+head -n1 message.pp > noroot.pp
+
+#Check the PermitRoot Login in SSH
+sudo grep PermitRoot /etc/ssh/sshd_config
+
+#Edit the manifest
+vim noroot.pp
+
+# vim: set ft=ruby ts=2 sw=2 et ai :
+service { 'sshd':
+  ensure => 'running',
+  enable => true,
+ }
+file_line { 'root_ssh':
+  path => '/etc/ssh/sshd_config',
+  ensure => 'present',
+  line => 'PermitRootLogin no',
+  match => '^PermitRootLogin',
+  notify => Service['sshd'],
+ }
+
+#Save and exit
+
+#Check the syntax
+puppet parser validate noroot.pp
+
+#Check the PermitRoot Login in SSH
+sudo grep PermitRoot /etc/ssh/sshd_config
